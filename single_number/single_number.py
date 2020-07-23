37d020e28f852c45800a65d98aa507e17e5388e1
Tate Keller <tatek1993@gmail.com>
2020-07-22 23:33:58 -0500

    single number test passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,22 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
+    # sort the array
+    arr.sort()
+    # the index
+    i = 0
 
-    pass
+    # loop through the array after we've sorted it
+    while i < len(arr):
+        # if the index is equal to the next index, then skip the next index because they are duplicates
+        if arr[i] == arr[i + 1]:
+            i += 2
+        elif arr[i] != arr[i + 1]:
+            return arr[i]
 
 
 if __name__ == '__main__':
